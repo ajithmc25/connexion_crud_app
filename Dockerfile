0c972b374787bc1137303b4fb366ed3553cb6190
ajith <ajith.m@scrapehero.com>
2022-05-10 13:58:55 +0530

    using mysql

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
-FROM python:3.7-alpine
+FROM python:3.8
 WORKDIR /code
-RUN apk add --no-cache gcc musl-dev linux-headers
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 EXPOSE 5000
--- connection.py
+++ connection.py
@@ -7,11 +7,10 @@ import connexion
 from flask_sqlalchemy import SQLAlchemy
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-db_path = "sqlite:////" + os.path.join(basedir, "user.db")
 
 connex_app = connexion.App(__name__, specification_dir=basedir)
 app = connex_app.app
 
-app.config["SQLALCHEMY_DATABASE_URI"] = db_path
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:passme@mysqldb/user'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
