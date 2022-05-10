967ca93926639cd25eb3bb59edb5ada023f6f90e
ajith <ajith.m@scrapehero.com>
2022-05-10 14:25:45 +0530

    conftest updated

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,29 +1,26 @@
-import os
-
-import connexion
 import pytest
-from flask_sqlalchemy import SQLAlchemy
 
-from connection import basedir
+from connection import connex_app
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='module')
 def client():
-    app = connexion.App(__name__, specification_dir=basedir)
-    # using a separate db for test to preserve main db
-    app.app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////" + os.path.join(basedir, "test.db")
-    app.app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    test_app = connex_app
 
-    db = SQLAlchemy(app.app)
-    app.app.config["TESTING"] = True
-    app.app.testing = True
+    # configure the DB
+    # in-memory sqlite DB for development purposes, will need file backing for persistence
+    test_app.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    test_app.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    client = app.app.test_client()
-    app.add_api('swagger.yaml')
-    db.create_all()
-    db.session.commit()
-    yield client
-    # removing test db to get fresh db next time the tests are ran
-    db.session.remove()
-    db.drop_all()
-    os.remove('test.db')
+    from connection import db
+    db.init_app(test_app.app)
+    test_app.add_api('swagger.yaml')
+
+    # create DB tables
+    with test_app.app.app_context():
+        db.create_all()
+
+    test_app.app.config['DEBUG'] = True
+    test_app.app.config['TESTING'] = True
+    with test_app.app.test_client() as c:
+        yield c
